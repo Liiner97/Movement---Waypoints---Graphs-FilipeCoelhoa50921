885e9ce153c2c39afd3131c52ce467ce774b364a
Liiner97 <coelhitohg@gmail.com>
2024-11-07 19:58:37 +0000

    Following a Circuit

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -1,37 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class FollowWP : MonoBehaviour {
 
-    public GameObject [] waypoints;
+    public GameObject[] waypoints;
     int currentWP = 0;
+
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+    void Update() {
+
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3.0f) {
+
             currentWP++;
+        }
 
-        if(currentWP >= waypoints.Length)
+        if (currentWP >= waypoints.Length) {
+
             currentWP = 0;
+        }
 
-        //this.transform.LookAt(waypoints[currentWP].transform);
+        // this.transform.LookAt(waypoints[currentWP].transform);
 
-        Quaternion lookatWp = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-
-        this.transform.rotation = Quaternion.Slerp(transform.rotation, lookatWp, rotSpeed * Time.deltaTime);
-
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        Quaternion lookAtWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, lookAtWP, Time.deltaTime * rotSpeed);
+        this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
     }
 }
